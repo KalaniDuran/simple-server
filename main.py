776fe923aac12cf77420867b3d4f2d72fd23aa3c
KalaniDuran <kduran@g.hmc.edu>
2022-04-09 17:39:56 -0500

    Added /skeleton path

--- main.py
+++ main.py
@@ -1,16 +1,43 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import time
+import os
 
 hostName = 'localhost'
 serverPort = 8080
 
 class MyHandler(BaseHTTPRequestHandler):
     def do_GET(self):
-        self.send_response(200)
-        self.send_header("Content-type", "text/html")
-        self.end_headers()
-        self.wfile.write(bytes("<html><p>Request: %s</p></html>" % self.path, "utf-8"))
+        match self.path:
+            case "/ping":
+                self.ping()
+            case "/skeleton":
+                self.skeleton()
+            case _:
+                self.default()
+            
 
+    def ping(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/plain")
+        self.end_headers()
+        self.wfile.write(bytes("pong", "utf-8"))
+    
+    def default(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/plain")
+        self.end_headers()
+        self.wfile.write(bytes("Request: %s" % self.path, "utf-8"))
+
+    def skeleton(self):
+        self.send_response(200)
+        self.send_header("Content-type", "image/gif")
+        self.end_headers()
+        self.wfile.write(MyHandler.load(os.getcwd() + "/images/skelly.gif"))
+    
+    def load(file):
+        with open(file, 'rb') as file_handle:
+            return file_handle.read()
+        # skelly
 
 if __name__ == '__main__':
     webServer = HTTPServer((hostName, serverPort), MyHandler)
