73098c978ae75ff689e87a7d676ae7ecddf2ae00
KalaniDuran <kduran@g.hmc.edu>
2022-04-19 18:25:45 -0500

    implemented basic getUser with json input

--- main.py
+++ main.py
@@ -3,13 +3,26 @@ import time
 import os
 import json
 import cgi
+import sqlite3
 
 hostName = 'localhost'
 serverPort = 8080
+connection = sqlite3.connect("/Users/Kalani/test.db")
+connection.row_factory = sqlite3.Row
 
 class MyHandler(BaseHTTPRequestHandler):
     def do_POST(self):
-        ctype, _ = cgi.parse_header(self.headers.get('content-type'))
+        match self.path:
+            case "/createuser":
+                pass
+            case "/createbook":
+                pass
+            case "/assignbook":
+                pass
+            case _:
+                self.default()
+        
+        """ ctype, _ = cgi.parse_header(self.headers.get('content-type'))
         
         # refuse to receive non-json content
         if ctype != 'application/json':
@@ -23,7 +36,7 @@ class MyHandler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header('Content-type', 'application/json')
         self.end_headers()
-        self.wfile.write(json.dumps(message).encode('utf-8'))
+        self.wfile.write(json.dumps(message).encode('utf-8')) """
 
     def do_GET(self):
         match self.path:
@@ -31,9 +44,46 @@ class MyHandler(BaseHTTPRequestHandler):
                 self.ping()
             case "/skeleton":
                 self.skeleton()
+            case "/getuser":
+                self.getUser() 
+            case "/getbook":
+                pass
+            case "/getbooksread":
+                pass
             case _:
                 self.default()
-            
+
+    def getUser(self):
+        ctype, _ = cgi.parse_header(self.headers.get('content-type'))
+        
+        # refuse to receive non-json content
+        if ctype != 'application/json':
+            self.send_response(400)
+            self.end_headers()
+            return
+
+        length = int(self.headers.get('content-length'))
+        message = json.loads(self.rfile.read(length))
+
+        #print(type(message))
+        #print(dict(message))
+
+        result = connection.execute("""
+            SELECT reader_id, name
+            FROM readers 
+            WHERE reader_id=:reader_id
+            AND name=:name""", message).fetchall()
+
+        print(type(result))
+
+        for row in result:
+            print(dict(row))
+
+        self.send_response(200)
+        self.send_header('Content-type', 'application/json')
+        self.end_headers()
+        for row in result:
+            self.wfile.write(json.dumps(dict(row)).encode('utf-8'))
 
     def ping(self):
         self.send_response(200)
